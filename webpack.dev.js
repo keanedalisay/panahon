02fd5cae4338f546f5a5bc1fef7f29f0b659ef8e
Keane Dalisay <keane.dalisay0118@gmail.com>
2022-12-22 17:05:24 +0800

    Configure webpack for dev and prod

---
+++ webpack.dev.js
@@ -0,0 +1,36 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    app: path.resolve(__dirname, "src/scripts/app.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  plugins: [new HTMLWebpackPlugin({ template: "src/index.temp.html" })],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        use: { loader: "html-loader", options: { minimize: false } },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(woff|woff2|ttf|eof|otf)$/i,
+        type: "asset/resource",
+        generator: { filename: "assets/fonts/[name][ext]" },
+      },
+      {
+        test: /\.(jpeg|jpg|png|webp|svg)$/i,
+        type: "asset/resource",
+        generator: { filename: "assets/images/[name][ext" },
+      },
+    ],
+  },
+};
---
+++ webpack.prod.js
@@ -0,0 +1,45 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    app: path.resolve(__dirname, "src/scripts/app.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[contenthash][ext]",
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: "[name].[contenthash][ext]",
+      template: "src/index.temp.html",
+    }),
+    new MiniCssExtractPlugin({ filename: "[name].[contenthash][ext]" }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eof)$/i,
+        type: "asset/resource",
+        generator: { filename: "assets/fonts/[name].[contenthash][ext]" },
+      },
+      {
+        test: /\.(jpeg|jpg|png|svg|webp)$/i,
+        type: "asset/resource",
+        generator: { filename: "assets/images/[name].[contenthash][ext]" },
+      },
+    ],
+  },
+};
