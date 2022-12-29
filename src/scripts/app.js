966282a718f7346ce9baa21d4e60e309d9ab8dab
Keane Dalisay <keane.dalisay0118@gmail.com>
2022-12-29 08:59:27 +0800

    Add toggle overlay and alert panel functionality

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,8 +4,29 @@ import commonStyle from "../styles/common.scss";
 import mainStyle from "../styles/style.scss";
 
 const App = (() => {
+  const overlay = document.querySelector("[data-slctr=overlay]");
+  const closeAlertBtn = document.querySelector("[data-slctr=closeAlertBtn]");
+
+  const toggleOverlay = () => {
+    overlay.classList.toggle("elem-hide");
+  };
+  const toggleAlertPanel = () => {
+    const alertPanel = document.querySelector("[data-slctr=alertPanel]");
+    alertPanel.classList.toggle("elem-hide");
+  };
+
   const init = () => {
     Anim.init();
+
+    closeAlertBtn.addEventListener("click", () => {
+      toggleOverlay();
+      toggleAlertPanel();
+    });
+
+    overlay.addEventListener("click", () => {
+      toggleOverlay();
+      toggleAlertPanel();
+    });
   };
 
   return { init };
