4ecd564be2a1469fceadcbb56d531f540aeecc6d
Keane Dalisay <keane.dalisay0118@gmail.com>
2023-01-01 14:06:03 +0800

    Add feature to get todays weather with geo loc API

--- src/scripts/helpers.js
+++ src/scripts/helpers.js
@@ -1,3 +1,21 @@
+import clearDayIcon from "../assets/svgs/clear-sky-day-icon.svg";
+import clearNightIcon from "../assets/svgs/clear-sky-night-icon.svg";
+
+import fewCloudsDayIcon from "../assets/svgs/few-clouds-day-icon.svg";
+import fewCloudsNightIcon from "../assets/svgs/few-clouds-night-icon.svg";
+
+import scatteredCloudsIcon from "../assets/svgs/scattered-clouds-icon.svg";
+import brokenCloudsIcon from "../assets/svgs/broken-clouds-icon.svg";
+
+import showerRainDayIcon from "../assets/svgs/shower-rain-day-icon.svg";
+import showerRainNightIcon from "../assets/svgs/shower-rain-night-icon.svg";
+
+import rainIcon from "../assets/svgs/rain-icon.svg";
+import snowIcon from "../assets/svgs/snow-icon.svg";
+
+import thunderstormIcon from "../assets/svgs/thunderstorm-icon.svg";
+import windCloudIcon from "../assets/svgs/wind-cloud-icon.svg";
+
 export const convToHex = (num) => {
   if (num < 16) return "00";
 
@@ -11,6 +29,11 @@ export const clampRgbVal = (val, maxVal) => {
   return val;
 };
 
+export const convToDbleDgt = (num) => {
+  if (num.toString().length < 2) return `0${num}`;
+  return num;
+};
+
 export const elemIsNotHidden = (elem) => {
   if (elem.classList.contains("elem-hide")) return false;
   return true;
@@ -19,3 +42,54 @@ export const elemIsNotHidden = (elem) => {
 export const storeToLocal = (key, value) => localStorage.setItem(key, value);
 export const getLocalKey = (key) => localStorage.getItem(key);
 export const deleteLocalKey = (key) => localStorage.removeItem(key);
+
+export const parseDegDrctn = (deg) => {
+  if (deg > 0 && deg < 49) return "NNE";
+  if (deg === 50) return "NE";
+  if (deg > 50 && deg < 89) return "ENE";
+  if (deg === 90) return "E";
+  if (deg > 90 && deg < 129) return "ESE";
+  if (deg === 130) return "SE";
+  if (deg > 130 && deg < 180) return "SSE";
+  if (deg === 180) return "S";
+  if (deg > 180 && deg < 230) return "SSW";
+  if (deg === 230) return "SW";
+  if (deg > 230 && deg < 270) return "WSW";
+  if (deg === 270) return "W";
+  if (deg > 270 && deg < 310) return "WNW";
+  if (deg === 310) return "NW";
+  if (deg > 310 && deg < 360) return "NNW";
+
+  return "N";
+};
+
+export const getWeatherIconPath = (string, hours) => {
+  let iconPath;
+
+  if (/clear sky/i.test(string)) {
+    iconPath = hours > 17 ? clearNightIcon : clearDayIcon;
+  } else if (
+    /mist/i.test(string) ||
+    /fog/i.test(string) ||
+    /smoke/i.test(string) ||
+    /haze/i.test(string) ||
+    /sand/i.test(string) ||
+    /dust/i.test(string) ||
+    /ash/i.test(string) ||
+    /squall/i.test(string) ||
+    /tornado/i.test(string)
+  ) {
+    iconPath = windCloudIcon;
+  } else if (/few clouds/i.test(string)) {
+    iconPath = hours > 17 ? fewCloudsNightIcon : fewCloudsDayIcon;
+  } else if (/scattered clouds/i.test(string)) iconPath = scatteredCloudsIcon;
+  else if (/broken clouds/i.test(string) || /overcast clouds/i.test(string)) {
+    iconPath = brokenCloudsIcon;
+  } else if (/thunderstorm/i.test(string)) iconPath = thunderstormIcon;
+  else if (/snow/i.test(string) || /sleet/.test(string)) iconPath = snowIcon;
+  else if (/shower rain/i.test(string) || /drizzle/.test(string)) {
+    iconPath = hours > 17 ? showerRainNightIcon : showerRainDayIcon;
+  } else if (/rain/i.test(string)) iconPath = rainIcon;
+
+  return iconPath;
+};
--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -1,23 +1,150 @@
-import { deleteLocalKey } from "./helpers";
+import {
+  isToday,
+  toDate,
+  secondsToMilliseconds,
+  getMinutes,
+  getHours,
+  differenceInHours,
+  parseISO,
+} from "date-fns";
+
+import {
+  convToDbleDgt,
+  deleteLocalKey,
+  getWeatherIconPath,
+  parseDegDrctn,
+} from "./helpers";
 
 const Weather = (() => {
-  let latPos;
-  let longPos;
-
   const Geo = navigator.geolocation ? navigator.geolocation : false;
 
-  const getLatAndLong = (pos) => {
+  const setTempLabels = (frcst) => {
+    const mainTempLabel = document.querySelector("[data-slctr=mainTempLabel]");
+    const maxTempLabel = document.querySelector("[data-slctr=maxTempLabel]");
+    const minTempLabel = document.querySelector("[data-slctr=minTempLabel]");
+    const feelsTempLabel = document.querySelector(
+      "[data-slctr=feelsTempLabel]"
+    );
+
+    mainTempLabel.textContent = `${Math.floor(frcst.main.temp)}°C`;
+    maxTempLabel.textContent = `${Math.floor(frcst.main.temp_max)}°C`;
+    minTempLabel.textContent = `${Math.floor(frcst.main.temp_min)}°C`;
+    feelsTempLabel.textContent = `${Math.floor(frcst.main.feels_like)}°C`;
+  };
+
+  const setWeather = (frcst) => {
+    const mainWeather = document.querySelector("[data-slctr=mainWeather]");
+    const forecastHours = getHours(parseISO(frcst.dt_txt));
+    const mainWeatherIconPath = getWeatherIconPath(
+      frcst.weather[0].description,
+      forecastHours
+    );
+
+    mainWeather.setAttribute("data", mainWeatherIconPath);
+    mainWeather.setAttribute("aria-label", frcst.weather[0].description);
+  };
+
+  const setWeatherLocation = (city) => {
+    const weatherLocLabel = document.querySelector(
+      "[data-slctr=weatherLocLabel]"
+    );
+    weatherLocLabel.textContent = `${city.name}, ${city.country}`;
+  };
+
+  const setWeatherDetails = (frcst) => {
+    const rainVolLabel = document.querySelector("[data-slctr=rainVolLabel]");
+    const humidityLabel = document.querySelector("[data-slctr=humidityLabel]");
+    const windSpeedLabel = document.querySelector(
+      "[data-slctr=windSpeedLabel]"
+    );
+    const windDirLabel = document.querySelector("[data-slctr=windDirLabel]");
+
+    const hasRainProp = Object.prototype.hasOwnProperty.call(frcst, "rain");
+
+    rainVolLabel.textContent = `${hasRainProp ? frcst.rain["3h"] : 0} mm`;
+    humidityLabel.textContent = `${frcst.main.humidity}%`;
+    windSpeedLabel.textContent = `${frcst.wind.speed} m/s`;
+    windDirLabel.textContent = `${parseDegDrctn(frcst.wind.deg)}`;
+  };
+
+  const setSuntime = (city) => {
+    const sunriseTimeLabel = document.querySelector(
+      "[data-slctr=sunriseTimeLabel]"
+    );
+    const sunsetTimeLabel = document.querySelector(
+      "[data-slctr=sunsetTimeLabel]"
+    );
+
+    const sunriseDate = toDate(secondsToMilliseconds(city.sunrise));
+    const sunsetDate = toDate(secondsToMilliseconds(city.sunset));
+
+    const sunriseTime = `${convToDbleDgt(
+      getHours(sunriseDate)
+    )} : ${convToDbleDgt(getMinutes(sunriseDate))}`;
+    const sunsetTime = `${convToDbleDgt(
+      getHours(sunsetDate)
+    )} : ${convToDbleDgt(getMinutes(sunsetDate))}`;
+
+    sunriseTimeLabel.textContent = sunriseTime;
+    sunsetTimeLabel.textContent = sunsetTime;
+  };
+
+  const setTodaysWeather = (frcst, city) => {
+    setTempLabels(frcst);
+    setWeather(frcst);
+    setWeatherLocation(city);
+    setWeatherDetails(frcst);
+    setSuntime(city);
+  };
+
+  const retrieveData = async (latitude, longitude) => {
+    const targetData = await fetch(`No API for you!`);
+    return targetData.json();
+  };
+
+  const forecastToday = (data) => {
+    let frcstToday;
+
+    for (let i = 0; i < data.list.length; i += 1) {
+      const forecast = data.list[i];
+      const timeNow = toDate(Date.now());
+      const forecastTime = parseISO(forecast.dt_txt);
+      if (
+        isToday(forecastTime) &&
+        differenceInHours(timeNow, forecastTime) < 3
+      ) {
+        frcstToday = forecast;
+        break;
+      }
+    }
+
+    console.log(data);
+    console.log(frcstToday);
+
+    setTodaysWeather(frcstToday, data.city);
+  };
+
+  const getLatAndLong = async (pos) => {
     deleteLocalKey("geoLocErr");
 
-    latPos = pos.coords.latitude;
-    longPos = pos.coords.longitude;
+    const latPos = pos.coords.latitude;
+    const longPos = pos.coords.longitude;
+
+    const data = await retrieveData(latPos, longPos);
+    forecastToday(data);
   };
 
   const getPosition = (rejectCall) => {
     Geo.getCurrentPosition(getLatAndLong, rejectCall, { timeout: 10000 });
   };
 
-  return { getPosition, Geo, latPos, longPos };
+  const obj = {
+    getPosition,
+    retrieveData,
+    Geo,
+  };
+
+  return obj;
 })();
 
 export default Weather;
